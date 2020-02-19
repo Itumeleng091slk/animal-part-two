1ee92fa03358462fa8f3cf9369243eb3165e8403
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-02-19 14:59:19 +0200

    Update animal_addingtest.py

--- animal_addingtest.py
+++ animal_addingtest.py
@@ -5,24 +5,24 @@ from dog import Dog
 
 class test_animal(unittest.TestCase):
     def setUp(self):
-        print('setUp')
+        print('Pass')
         self.cat_1 = Animal('Food','Meow')
         self.dog_1 = Animal('Food','Bark')
 
     def test_DogEats(self):
-        print('test_DogandCat_Eats')
+        print('Does dog eat food')
         self.assertEqual(self.cat_1.eats,'Food')
 
     def test_CatEats(self):
-        print('Cat_Eats')
+        print('Does cat eat food')
         self.assertEqual(self.dog_1.eats,'Food')
 
     def test_DogSound(self):
-        print('test_DogandCat_Sound')
+        print('Does the dog bark')
         self.assertEqual(self.dog_1.sounds,'Bark')
     
     def test_CatSound(self):
-        print('test_CatSound')
+        print('Does the cat meow')
         self.assertEqual(self.cat_1.sounds,'Meow')
 
 if __name__ == "__main__":
